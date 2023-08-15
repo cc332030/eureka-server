3462ca516d92e307a04ca639fbf5e6ab22024332
c332030 <c332030@c332030.com>
2023-08-15 16:03:51 +0800

    feat: version

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,12 +5,10 @@ import org.springframework.boot.gradle.tasks.bundling.BootJar
 
 plugins {
     id("org.springframework.boot") version "3.1.2"
-    id("io.spring.dependency-management") version "1.1.2"
-    kotlin("jvm") version "1.8.22"
-    kotlin("plugin.spring") version "1.8.22"
+    id("io.spring.dependency-management") version "1.1.3"
+    kotlin("jvm") version "1.9.0"
+    kotlin("plugin.spring") version "1.9.0"
 }
-
-
 
 dependencies {
 
